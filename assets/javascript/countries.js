a19bf46ec2afc75fc2e48525881663848163bc86
Leevie Martin <leevie@gmail.com>
2019-03-18 16:11:57 -0500

    Added my changes - prod not touched

---
+++ assets/javascript/countries.js
@@ -0,0 +1,316 @@
+const  countryObj = [
+  {
+    country: "Argentina",
+    p_id: "37i9dQZEVXbMMy2roB9myp"
+  },
+  {
+    country: "Australia",
+    p_id: "37i9dQZEVXbJPcfkRz0wJ0"
+  },
+  {
+    country: "Austria",
+    p_id: "37i9dQZEVXbKNHh6NIXu36"
+  },
+  {
+    country: "Belgium",
+    p_id: "37i9dQZEVXbJNSeeHswcKB"
+  },
+  {
+    country: "Bolivia",
+    p_id: "37i9dQZEVXbJqfMFK4d691"
+  },
+  {
+    country: "Brazil",
+    p_id: "37i9dQZEVXbMXbN3EUUhlg"
+  },
+  {
+    country: "Bulgaria",
+    p_id: "37i9dQZEVXbNfM2w2mq1B8"
+  },
+  {
+    country: "Canada",
+    p_id: "37i9dQZEVXbKj23U1GF4IR"
+  },
+  {
+    country: "Chile",
+    p_id: "37i9dQZEVXbL0GavIqMTeb"
+  },
+  {
+    country: "Colombia",
+    p_id: "37i9dQZEVXbOa2lmxNORXQ"
+  },
+  {
+    country: "Costa Rica",
+    p_id: "37i9dQZEVXbMZAjGMynsQX"
+  },
+  {
+    country: "Czech Republic",
+    p_id: "37i9dQZEVXbIP3c3fqVrJY"
+  },
+  {
+    country: "Denmark",
+    p_id: "37i9dQZEVXbL3J0k32lWnN"
+  },
+  {
+    country: "Dominican Republic",
+    p_id: "37i9dQZEVXbKAbrMR8uuf7"
+  },
+  {
+    country: "Ecuador",
+    p_id: "37i9dQZEVXbJlM6nvL1nD1"
+  },
+  {
+    country: "El Salvador",
+    p_id: "37i9dQZEVXbLxoIml4MYkT"
+  },
+  {
+    country: "Estonia",
+    p_id: "37i9dQZEVXbLesry2Qw2xS"
+  },
+  {
+    country: "Finland",
+    p_id: "37i9dQZEVXbMxcczTSoGwZ"
+  },
+  {
+    country: "France",
+    p_id: "37i9dQZEVXbIPWwFssbupI"
+  },
+  {
+    country: "Germany",
+    p_id: "37i9dQZEVXbJiZcmkrIHGU"
+  },
+  {
+    country: "Greece",
+    p_id: "37i9dQZEVXbJqdarpmTJDL"
+  },
+  {
+    country: "Guatemala",
+    p_id: "37i9dQZEVXbLy5tBFyQvd4"
+  },
+  {
+    country: "Honduras",
+    p_id: "37i9dQZEVXbJp9wcIM9Eo5"
+  },
+  {
+    country: "Hong Kong",
+    p_id: "37i9dQZEVXbLwpL8TjsxOG"
+  },
+  {
+    country: "Hungary",
+    p_id: "37i9dQZEVXbNHwMxAkvmF8"
+  },
+  {
+    country: "Iceland",
+    p_id: "37i9dQZEVXbKMzVsSGQ49S"
+  },
+  {
+    country: "India",
+    p_id: "37i9dQZEVXbLZ52XmnySJg"
+  },
+  {
+    country: "Indonesia",
+    p_id: "37i9dQZEVXbObFQZ3JLcXt"
+  },
+  {
+    country: "Ireland",
+    p_id: "37i9dQZEVXbKM896FDX8L1"
+  },
+  {
+    country: "Israel",
+    p_id: "37i9dQZEVXbJ6IpvItkve3"
+  },
+  {
+    country: "Italy",
+    p_id: "37i9dQZEVXbIQnj7RRhdSX"
+  },
+  {
+    country: "Japan",
+    p_id: "37i9dQZEVXbKXQ4mDTEBXq"
+  },
+  {
+    country: "Latvia",
+    p_id: "37i9dQZEVXbJWuzDrTxbKS"
+  },
+  {
+    country: "Lithuania",
+    p_id: "37i9dQZEVXbMx56Rdq5lwc"
+  },
+  {
+    country: "Luxembourg",
+    p_id: "37i9dQZEVXbKGcyg6TFGx6"
+  },
+  {
+    country: "Malaysia",
+    p_id: "37i9dQZEVXbJlfUljuZExa"
+  },
+  {
+    country: "Malta",
+    p_id: "37i9dQZEVXbMD2H5HJqmx9"
+  },
+  {
+    country: "Mexico",
+    p_id: "37i9dQZEVXbO3qyFxbkOE1"
+  },
+  {
+    country: "Netherlands",
+    p_id: "37i9dQZEVXbKCF6dqVpDkS"
+  },
+  {
+    country: "New Zealand",
+    p_id: "37i9dQZEVXbM8SIrkERIYl"
+  },
+  {
+    country: "Nicaragua",
+    p_id: "37i9dQZEVXbISk8kxnzfCq"
+  },
+  {
+    country: "Norway",
+    p_id: "37i9dQZEVXbJvfa0Yxg7E7"
+  },
+  {
+    country: "Panama",
+    p_id: "37i9dQZEVXbKypXHVwk1f0"
+  },
+  {
+    country: "Paraguay",
+    p_id: "37i9dQZEVXbNOUPGj7tW6T"
+  },
+  {
+    country: "Peru",
+    p_id: "37i9dQZEVXbJfdy5b0KP7W"
+  },
+  {
+    country: "Philippines",
+    p_id: "37i9dQZEVXbNBz9cRCSFkY"
+  },
+  {
+    country: "Poland",
+    p_id: "37i9dQZEVXbN6itCcaL3Tt"
+  },
+  {
+    country: "Portugal",
+    p_id: "37i9dQZEVXbKyJS56d1pgi"
+  },
+  {
+    country: "Romania",
+    p_id: "37i9dQZEVXbNZbJ6TZelCq"
+  },
+  {
+    country: "Singapore",
+    p_id: "37i9dQZEVXbK4gjvS1FjPY"
+  },
+  {
+    country: "Slovakia",
+    p_id: "37i9dQZEVXbKIVTPX9a2Sb"
+  },
+  {
+    country: "South Africa",
+    p_id: "37i9dQZEVXbMH2jvi6jvjk"
+  },
+  {
+    country: "Spain",
+    p_id: "37i9dQZEVXbNFJfN1Vw8d9"
+  },
+  {
+    country: "Sweden",
+    p_id: "37i9dQZEVXbLoATJ81JYXz"
+  },
+  {
+    country: "Switzerland",
+    p_id: "37i9dQZEVXbJiyhoAPEfMK"
+  },
+  {
+    country: "Taiwan",
+    p_id: "37i9dQZEVXbMnZEatlMSiu"
+  },
+  {
+    country: "Thailand",
+    p_id: "37i9dQZEVXbMnz8KIWsvf9"
+  },
+  {
+    country: "Turkey",
+    p_id: "37i9dQZEVXbIVYVBNw9D5K"
+  },
+  {
+    country: "United Kingdom",
+    p_id: "37i9dQZEVXbLnolsZ8PSNw"
+  },
+  {
+    country: "United States",
+    p_id: "37i9dQZEVXbLRQDuF5jeBp"
+  },
+  {
+    country: "Uruguay",
+    p_id: "37i9dQZEVXbMJJi3wgRbAy"
+  },
+  {
+    country: "Vietnam",
+    p_id: "37i9dQZEVXbLdGSmz6xilI"
+  }
+];
+
+
+// In case we need this list for later:
+
+{/* <option>Argentina</option>
+<option>Australia</option>
+<option>Austria</option>
+<option>Belgium</option>
+<option>Bolivia</option>
+<option>Brazil</option>
+<option>Bulgaria</option>
+<option>Canada</option>
+<option>Chile</option>
+<option>Colombia</option>
+<option>Costa Rica</option>
+<option>Czech Republic</option>
+<option>Denmark</option>
+<option>Dominican Republic</option>
+<option>Ecuador</option>
+<option>El Salvador</option>
+<option>Estonia</option>
+<option>Finland</option>
+<option>France</option>
+<option>Germany</option>
+<option>Greece</option>
+<option>Guatemala</option>
+<option>Honduras</option>
+<option>Hong Kong</option>
+<option>Hungary</option>
+<option>Iceland</option>
+<option>India</option>
+<option>Indonesia</option>
+<option>Ireland</option>
+<option>Israel</option>
+<option>Italy</option>
+<option>Japan</option>
+<option>Latvia</option>
+<option>Lithuania</option>
+<option>Luxembourg</option>
+<option>Malaysia</option>
+<option>Malta</option>
+<option>Mexico</option>
+<option>Netherlands</option>
+<option>New Zealand</option>
+<option>Nicaragua</option>
+<option>Norway</option>
+<option>Panama</option>
+<option>Paraguay</option>
+<option>Peru</option>
+<option>Philippines</option>
+<option>Poland</option>
+<option>Portugal</option>
+<option>Romania</option>
+<option>Singapore</option>
+<option>Slovakia</option>
+<option>South Africa</option>
+<option>Spain</option>
+<option>Sweden</option>
+<option>Switzerland</option>
+<option>Taiwan</option>
+<option>Thailand</option>
+<option>Turkey</option>
+<option>United Kingdom</option>
+<option>United States</option>
+<option>Uruguay</option>
+<option>Vietnam</option> */}
